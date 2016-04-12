b4546c19b98cdde1a312b28bd36313533bd6d088
Steve Hansell <shansell@gannett.com>
2016-04-12 12:38:53 -0400

    Fixed some formatting in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -38,21 +38,21 @@ function set_prompt_symbol_color() {
 function git_prompt() {
   ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
-	echo "%{$fg_bold[cyan]%}[✭ ${ref#refs/heads/}]%{$reset_color%}"
+	echo "%{$fg_bold[cyan]%}[✭ ${ref#refs/heads/}]%{$reset_color%} "
 }
 
 
 export VIRTUAL_ENV_DISABLE_PROMPT=yes
 function set_pyenv_prompt() {
 	if [ $VIRTUAL_ENV ]; then
-		echo "%{$fg_bold[green]%}[ξ `basename $VIRTUAL_ENV`]%{$reset_color%}"
+		echo "%{$fg_bold[green]%}[ξ `basename $VIRTUAL_ENV`]%{$reset_color%} "
 	fi
 }
 
 
 function set_rbenv_prompt() {
 	if which rbenv &> /dev/null; then
-		echo "%{$fg_bold[red]%}[♦ $(rbenv version | sed -e "s/ (set.*$//")]%{$reset_color%}"
+		echo "%{$fg_bold[red]%}[♦ $(rbenv version | sed -e "s/ (set.*$//")]%{$reset_color%} "
 	fi
 }
 
@@ -61,11 +61,11 @@ local current_dir='%{$terminfo[bold]$fg[white]%}$(collapse_pwd)%{$reset_color%}'
 local prompt_color='$(set_prompt_symbol_color)'
 local prompt_symbol="${prompt_color}❯❯❯%{$reset_color%}"
 local git_branch='$(git_prompt)'
-local pyenv_prompt="$(set_pyenv_prompt)"
-local rbenv_prompt="$(set_rbenv_prompt)"
+local pyenv_prompt='$(set_pyenv_prompt)'
+local rbenv_prompt='$(set_rbenv_prompt)'
 
 PROMPT="
-${current_dir} ${git_branch} ${pyenv_prompt} ${rbenv_prompt}
+${current_dir} ${git_branch}${pyenv_prompt}${rbenv_prompt}
 ${prompt_symbol} "
 
 RPS1="${return_code}"
